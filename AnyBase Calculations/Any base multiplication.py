bb2ebeb02538695ccb18aa7d722865b2611dcd05
VishnuKumarSS <vishnukumarss941@gmail.com>
2022-12-13 23:14:12 +0530

    Pushed Any base multiplication.py

---
+++ AnyBase Calculations/Any base multiplication.py
@@ -0,0 +1,60 @@
+def AnyToDec(n, base):
+    reversed_n = str(n)[::-1]
+    count=0
+    answer = 0
+    for i in reversed_n:
+        answer += int(i) * base ** count
+        count += 1
+    return answer
+
+def DecToAny(n, base):
+    remainders = ""
+    while n>0:
+        remainderr = n % base
+        n = n//base
+        remainders += str(remainderr)
+    answer = int(remainders[::-1])
+    return answer
+    
+def getProduct(b, n1, n2):
+    num1 = AnyToDec(n1, b)
+    num2 = AnyToDec(n2, b)
+    num = num1 * num2
+    ans = DecToAny(num, b)
+    return int(ans)
+
+if __name__ == '__main__':
+    base = int(input())
+    n1 = int(input())
+    n2 = int(input())
+    
+    solution = getProduct(base, n1, n2)
+    print(solution)
+
+
+
+"""
+Any Base Multiplication
+Easy  Prev   Next
+1. You are given a base b.
+2. You are given two numbers n1 and n2 of base b.
+3. You are required to multiply n1 and n2 and print the value.
+Input Format
+A base b
+A number n1
+A number n2
+Output Format
+A number of base b equal in value to n2 * n1.
+
+
+Constraints
+2 <= b <= 10
+0 <= n1 <= 10000
+0 <= n2 <= 10000
+Sample Input
+5
+1220
+31
+Sample Output
+43320
+"""
